b46e7d421335c733bf300cfd0ba33bce8e68c631
danielaser <daniela.serrano.dev@gmail.com>
2025-01-21 00:51:56 -0500

    fix(develop): add ngfor to list in worker-panel component

--- src/app/components/worker-panel/worker-panel.component.ts
+++ src/app/components/worker-panel/worker-panel.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-worker-panel',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './worker-panel.component.html',
   styleUrl: './worker-panel.component.scss'
 })
 export class WorkerPanelComponent {
-
+  listItems = [
+    { title: 'Easy Installation', icon: 'tick' },
+    { title: 'Built-in Themes', icon: 'tick' },
+    { title: 'Light and Dark Mode', icon: 'tick' },
+    { title: 'App Store and Google Play Integration', icon: 'tick' },
+    { title: 'Custom Themes', icon: 'tick' },
+    { title: 'Custom Wallets List', icon: 'tick' },
+    { title: 'Custom Chains', icon: 'tick' },
+    { title: 'Custom Connect Button', icon: 'tick' }
+  ];
 }
