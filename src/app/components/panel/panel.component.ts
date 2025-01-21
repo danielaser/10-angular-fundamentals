94605fb43d6accf8d039e2403bc1e72fe00b6b0d
danielaser <daniela.serrano.dev@gmail.com>
2025-01-21 09:41:09 -0500

    feat(develop): add for to panel menu

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -1,11 +1,64 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-panel',
+  imports: [CommonModule],
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.scss']
 })
 export class PanelComponent implements OnChanges {
+  menus = [
+    {
+      name: 'Rainbow',
+      icon: 'svg/rainbow.svg#rainbow',
+      id: 'rainbow'
+    },
+    {
+      name: 'Coinbase Wallet',
+      icon: 'svg/coinbase.svg#coinbase',
+      id: 'coinbase'
+    },
+    {
+      name: 'MetaMask',
+      icon: 'svg/metamask.svg#metamask',
+      id: 'metamask'
+    },
+    {
+      name: 'Wallet Connect',
+      icon: 'svg/wallet.svg#wallet',
+      id: 'wallet'
+    }
+  ];
+
+  moreMenus = [
+    {
+      name: 'Argent',
+      icon: 'svg/argent.svg#argent',
+      id: 'argent'
+    },
+    {
+      name: 'Trust Wallet',
+      icon: 'svg/trust.svg#trust',
+      id: 'trust'
+    },
+    {
+      name: 'Omni',
+      icon: 'svg/omni.svg#omni',
+      id: 'omni'
+    },
+    {
+      name: 'imToken',
+      icon: 'svg/token.svg#token',
+      id: 'token'
+    },
+    {
+      name: 'Leader',
+      icon: 'svg/leader.svg#leader',
+      id: 'leader'
+    }
+  ];
+
   @Input() panelBackground: string = 'rgb(255, 255, 255)';
   @Input() panelRadius: string = '15px';
 
