833038fcae06d2b8b65a8b7bab54b93b3e2a3aca
danielaser <daniela.serrano.dev@gmail.com>
2025-01-21 09:53:56 -0500

    feat(develop): divide panel in small components

---
+++ src/app/components/header-panel/header-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-panel',
+  imports: [],
+  templateUrl: './header-panel.component.html',
+  styleUrl: './header-panel.component.scss'
+})
+export class HeaderPanelComponent {
+
+}
